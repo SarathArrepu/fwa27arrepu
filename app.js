fae07d11e49c3cb14dfb453ed760c8f249883687
SarathArrepu <a.sarath2090737@gmail.com>
2021-10-27 14:21:52 -0500

    updated app.js

--- app.js
+++ app.js
@@ -3,22 +3,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var mydataRouter = require('./routes/mydata');
 var indexRouter = require('./routes/index');
+var mydataRouter = require('./routes/mydata');
 var usersRouter = require('./routes/users');
-
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/mydata', mydataRouter)
+app.use('/', mydataRouter)
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
